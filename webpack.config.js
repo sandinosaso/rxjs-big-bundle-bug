10c6fc92654271fdcc87f918e97f4629a346d630
sandinosaso <sandinosaso@gmail.com>
2019-05-27 23:28:19 -0400

    Minimal reproduction of big bundle size for RxJS project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'lib');
+
+const config = {
+  entry: `${APP_DIR}/index.js`,
+  mode: 'development',
+  output: {
+    path: BUILD_DIR,
+    filename: 'launcher.js',
+    libraryTarget: 'umd',
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+};
+
+module.exports = config;
